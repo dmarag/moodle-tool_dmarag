80f5cab9e5ff7e99fb1ad260280a00297184adc5
dmarag <43951643+dmarag@users.noreply.github.com>
2018-10-11 16:41:22 +0300

    Navigation and settings

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
 require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
- 
+require_once($CFG->dirroot. '/admin/tool/dmarag/lib.php');
+
+global $DB;
 $courseid = required_param('id', PARAM_INT);
- 
+
+# get course object 
+//$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+//$context_course = context_course::instance($courseid);
+
 // Set up the page.
 $title = get_string('pluginname', 'tool_dmarag');
 $pagetitle = $title;
-$url = new moodle_url("/admin/tool/dmarag/index.php");
+//$url = new moodle_url("/admin/tool/dmarag/index.php");
+$url = new moodle_url('/admin/tool/dmarag/index.php', ['id' => $courseid]);
 $PAGE->set_url($url);
 $PAGE->set_title($title);
 $PAGE->set_heading($title);
---
+++ lib.php
@@ -0,0 +1,9 @@
+<?php
+
+function tool_dmarag_extend_navigation_course($navigation, $course, $context) 
+{
+    $navigation->add(
+        get_string('pluginname', 'tool_dmarag'),
+			new moodle_url('/admin/tool/dmarag/index.php', ['id' => $course->id]), navigation_node::TYPE_SETTING, 
+			get_string('pluginname', 'tool_dmarag'), 'dmarag', new pix_icon('icon', '', 'tool_dmarag'));
+}
