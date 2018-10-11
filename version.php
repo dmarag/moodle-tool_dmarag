8ff5cb5f67eb341557443dcb119edd3f023c9d0e
dmarag <43951643+dmarag@users.noreply.github.com>
2018-10-11 11:31:50 +0300

    tools files

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+// TEST - MY PLUGIN
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2018101100;  	// The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2018050800; 	// Requires this Moodle version.
+$plugin->component = 'tool_dmarag'; // Full name of the plugin (used for diagnostics).
